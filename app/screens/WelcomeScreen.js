bea6b9284dcbef58f8ef2f7e720b4405817ccafd
devanaclimgo <anaclimgo@gmail.com>
2025-03-17 20:05:34 -0300

    welsome screen styled

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import { ImageBackground, StyleSheet } from 'react-native'
+import React from 'react';
+import { ImageBackground, StyleSheet } from 'react-native';
 
-const WelcomeScreen = props => {
+const WelcomeScreen = (props) => {
   return (
-    <ImageBackground source={require("../assets/background.jpg")}></ImageBackground>
-  )
-}
+    <ImageBackground
+      style={styles.background}
+      source={require('../assets/background.jpg')}
+    ></ImageBackground>
+  );
+};
 
 const styles = StyleSheet.create({
-  
-})
+  background: {
+    flex: 1,
+  },
+});
 
-export default WelcomeScreen
+export default WelcomeScreen;
