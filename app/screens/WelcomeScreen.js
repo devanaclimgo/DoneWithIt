21fbc0a9a2ba45e2e1b453c181b7fc5adcc38f0b
devanaclimgo <anaclimgo@gmail.com>
2025-03-17 20:04:08 -0300

    welcome home screen main code add

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ImageBackground, StyleSheet } from 'react-native'
+
+const WelcomeScreen = props => {
+  return (
+    <ImageBackground source={require("../assets/background.jpg")}></ImageBackground>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+})
+
+export default WelcomeScreen
