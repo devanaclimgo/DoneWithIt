4bd2f055b9a9431299c000b21a21edf88688d595
devanaclimgo <anaclimgo@gmail.com>
2025-03-17 20:09:10 -0300

    login button added to welcome screen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { ImageBackground, StyleSheet, View } from 'react-native';
 
 const WelcomeScreen = (props) => {
   return (
     <ImageBackground
       style={styles.background}
       source={require('../assets/background.jpg')}
-    ></ImageBackground>
+    >
+      <View style={styles.loginButton}></View>
+    </ImageBackground>
   );
 };
 
@@ -14,6 +16,11 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
   },
+  loginButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#fc5c65'
+  }
 });
 
 export default WelcomeScreen;
